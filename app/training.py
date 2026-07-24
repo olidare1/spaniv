5e2adcd0240c0d9d3d41cad7b9084aa3013914a7
olidare1 <oli.dare.tech@gmail.com>
2026-07-24 19:06:00 +0200

    added a datetime task to imrpove database

--- app/training.py
+++ app/training.py
@@ -1,1 +1,8 @@
-print("H")
+from datetime import datetime, timezone, timedelta
+
+created_at = datetime.now(timezone.utc) 
+expiry_time = created_at + timedelta(minutes=7)  # Set expiry time to 7 days from creation
+
+print("Created at:", created_at)
+print("Expiry time:", expiry_time)  
+
